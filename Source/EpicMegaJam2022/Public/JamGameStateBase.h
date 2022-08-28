f0727205b13c500ebd636218f44ef0571ffb3836
Stephen321 <stephen.enniis@gmail.com>
2022-08-28 15:27:58 +0100

    #9 and #10. tokens stored on game state, can add from PlayerCharacter, test token trigger actors, token UI and general level HUD setup

--- Source/EpicMegaJam2022/Public/JamGameStateBase.h
+++ Source/EpicMegaJam2022/Public/JamGameStateBase.h
@@ -6,12 +6,29 @@
 #include "GameFramework/GameStateBase.h"
 #include "JamGameStateBase.generated.h"
 
+// bp accessible events for changes to game state
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTokensChanged, int32, Tokens);
+
 /**
- * 
+ * Seeing as there is only over going to be 1 player. We keep Balance and Tokens here
+ * Balance in particular is more relevant as it will determine other aspects of Gameplay
+ * Tokens *could* be kept on PlayerState but not necessary
  */
 UCLASS()
 class EPICMEGAJAM2022_API AJamGameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
-	
+
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Jam")
+	void AddTokens(int32 Count);
+
+public: // Delegates
+	UPROPERTY(BlueprintAssignable, Category="Jam")
+	FOnTokensChanged OnTokensChanged;
+
+private:
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess), Category="Jam")
+	int32 Tokens;
 };
